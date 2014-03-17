62347946b37a53b9ad9d75d6866fb1786f5feaf1
soundswarm <seantconnor@gmail.com>
2014-03-16 21:16:42 -0700

    initial commit after rename of project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Connectus::Application.config.secret_key_base = '485dacbe5835900c7fd20e5c98df976b9285860b81a815a2213e97635c6df9579388c958172ac4e89088c7995e318e96910ac588ada5e73dff76e31c2cfc3e73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Connectus::Application.config.session_store :cookie_store, key: '_connectus_session'
